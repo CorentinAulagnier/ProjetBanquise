e57c5748ba34cb17883be0f6f21f46ff0fce040c
CorentinAulagnier <corentinaulagnier@hotmail.fr>
2017-05-03 15:56:35 +0200

    Update Partie.java

--- src/model/Partie.java
+++ src/model/Partie.java
@@ -89,6 +89,50 @@ public class Partie {
 					return false;
 		return true;
 	}
+	
+	/**
+	 * Le joueur actif recupere les poissons de la case c
+	 */
+	
+	public void manger(Coordonnees c) {
+		Tuile t = b.getTuile(c);
+		
+		if (t.aUnPingouin) { //La case contient bien un pingouin
+			joueurs[joueurActif].poissonsManges += t.nbPoissons;
+			t. enlevePoissons();	
+			
+		} else {
+			System.out.println("Erreur : la case à manger ne contient pas de pingouin \n(tuile.aUnPingouin == false) \nCoordonnees : "+c);
+		}
+	}
 
+
+	/**
+	 * Deplace un pingouin de "dep" vers "arr"
+	 */
+	
+	public void deplacement(Coordonnees dep, Coordonnees arr) {
+		
+		manger(dep);
+		Tuile tuileDep = b.getTuile(dep);
+		Tuile tuileArr = b.getTuile(arr);
+		
+		//Recherche du pingouin a deplacer
+		int i;
+		for (i = 0; (i < joueurs[joueurActif].nbPingouin && !(joueurs[joueurActif].myPingouins[i].position.equals(dep))); i++)
+		
+		//On a trouve le pingouin, c'est le numero i
+		if (i < joueurs[joueurActif].nbPingouin) { 
+			
+			joueurs[joueurActif].myPingouins[i].position = arr;
+			joueurs[joueurActif].nbTuiles ++;
+			
+			tuileDep.enlevePingouin();
+			tuileArr.metrePingouin();
+			
+		} else {
+			System.out.println("Erreur : deplacement du pingouin impossible, pas de pingouin à la case : " + dep);
+		}
+	}
 }
 

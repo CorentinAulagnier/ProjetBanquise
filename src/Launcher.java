e2db6492835cd6dc7aebf9758a65ada163bba2f8
corentin <corentinaulagnier@hotmail.fr>
2017-05-04 13:39:31 +0200

    ajour launcher

---
+++ src/Launcher.java
@@ -0,0 +1,18 @@
+import model.*;
+import vue.*;
+import controleur.*;
+
+public class Launcher {
+	
+	public static void main(String[] args) {	
+
+		int hauteur = 6;
+		int largeur = 8;
+		
+		Partie p = new Partie();
+		//Interface.creer(args, p);
+	}
+
+		
+
+}

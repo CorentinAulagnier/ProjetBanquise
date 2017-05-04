4cd32e9fabef55b7f39562d9baea57e953dfe454
Melanie Carriere <carrieme@f202-05.e-im2ag.ujf-grenoble.fr>
2017-05-04 18:14:12 +0200

    ArbreGraphe avec commentaires

--- src/model/ArbreGraphe.java
+++ src/model/ArbreGraphe.java
@@ -1,15 +1,18 @@
 package model;
 
+/* Classe qui sert a representer le terrain sous forme d'un graphe
+ * Un noeud represente une Tuile qui a 6 noeuds qui sont les tuiles adjacentes sur la banquise
+ */
 public class ArbreGraphe {
     
-    ArbreGraphe noeudHD;
-    ArbreGraphe noeudMD;
-    ArbreGraphe noeudBD;
-    ArbreGraphe noeudBG;
-    ArbreGraphe noeudMG;
-    ArbreGraphe noeudHG;
-    Coordonnees c;
-    int poids;
+    ArbreGraphe noeudHD; // noeud en haut a droite de this
+    ArbreGraphe noeudMD; // noeud a droite de this
+    ArbreGraphe noeudBD; // noeud en bas a droite de this
+    ArbreGraphe noeudBG; // noeud en bas a gauche de this
+    ArbreGraphe noeudMG; // noeud a gauche de this
+    ArbreGraphe noeudHG; // noeud en haut a gauche de this
+    Coordonnees c;  // Coordonnees du noeud sur le terrain
+    int poids;      // Nombre de poissons presents sur la tuile
     
     public ArbreGraphe() {
         this.noeudHD = null;

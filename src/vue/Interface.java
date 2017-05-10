6158d372723af5b4f0bf3c47c596bdcb77d6a9d9
Noémi <Nomi@192.168.0.26>
2017-05-10 18:51:27 +0200

    rectification bouton accueil

--- src/vue/Interface.java
+++ src/vue/Interface.java
@@ -35,7 +35,7 @@ public class Interface extends Application {
 		gestionnaireEcransFXML.chargeEcran(model.Proprietes.ECRAN_REGLES, model.Proprietes.ECRAN_REGLES_FXML);
 		gestionnaireEcransFXML.chargeEcran(model.Proprietes.ECRAN_MODE, model.Proprietes.ECRAN_MODE_FXML);
         
-		gestionnaireEcransFXML.changeEcranCourant(model.Proprietes.ECRAN_MODE);
+		gestionnaireEcransFXML.changeEcranCourant(model.Proprietes.ECRAN_ACCUEIL);
         
         Group root = new Group();
         root.getChildren().addAll(gestionnaireEcransFXML);

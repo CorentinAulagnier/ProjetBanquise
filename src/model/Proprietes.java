92be376576a312c081a656de4afdce468e8b6ed2
kuhlkrein <superseb.07@hotmail.fr>
2017-05-10 21:54:41 +0200

    link de regle à revoir mais ça marche

--- src/model/Proprietes.java
+++ src/model/Proprietes.java
@@ -3,8 +3,8 @@ package model;
 public class Proprietes {
     public static final String ECRAN_ACCUEIL = "Menu";
     public static final String ECRAN_ACCUEIL_FXML = "fxml/Menu.fxml";
-    public static final String ECRAN_REGLES = "Page2";
-    public static final String ECRAN_REGLES_FXML = "fxml/Page2.fxml"; 
+    public static final String ECRAN_REGLES = "PremiereRegle";
+    public static final String ECRAN_REGLES_FXML = "fxml/PremiereRegle.fxml"; 
     public static final String ECRAN_MODE = "Mode";
     public static final String ECRAN_MODE_FXML = "fxml/ModeJeu.fxml";
     

8b99c0ebb33d05f5708c57efe8f7d60d185a5d49
noemi <nthissey@gmail.com>
2017-05-10 09:34:55 +0200

    maj chargeurfmxl

--- src/vue/Interface.java
+++ src/vue/Interface.java
@@ -1,17 +1,21 @@
 package vue;
 
 import model.Partie;
+
+import java.net.URL;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.Group;
 import javafx.stage.Stage;
 
 public class Interface extends Application {
 	
-	public static final String ECRAN_ACCUEIL = "accueil";
-    public static final String ECRAN_ACCUEIL_FXML = "accueil.fxml";
-    public static final String ECRAN_REGLES = "regles";
-    public static final String ECRAN_REGLES_FXML = "regles.fxml"; 
+	public static final String ECRAN_ACCUEIL = "Menu";
+    public static final String ECRAN_ACCUEIL_FXML = "fxml/Menu.fxml";
+    public static final String ECRAN_REGLES = "PremiereRegle";
+    public static final String ECRAN_REGLES_FXML = "fxml/PremiereRegle.fxml"; 
 	public static int largeurFenetre = 800;
 	public static int hauteurFenetre = 600;
 	public static Partie partie;
@@ -27,16 +31,20 @@ public class Interface extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-		InterfaceurFxml interfaceurPinguouins = new InterfaceurFxml();
-		interfaceurPinguouins.chargeEcran(Interface.ECRAN_ACCUEIL,Interface.ECRAN_ACCUEIL_FXML);
-		interfaceurPinguouins.chargeEcran(Interface.ECRAN_REGLES,Interface.ECRAN_REGLES_FXML);
-		interfaceurPinguouins.fixeEcran(Interface.ECRAN_ACCUEIL);
+		ChargeurFxml chargeurFxml1er= new ChargeurFxml();
+		chargeurFxml1er.chargeEcran(Interface.ECRAN_ACCUEIL,Interface.ECRAN_ACCUEIL_FXML);
+		//chargeurFxml1er.chargeEcran(Interface.ECRAN_REGLES,Interface.ECRAN_REGLES_FXML);
 		
-		Group root = new Group();
-		root.getChildren().addAll(interfaceurPinguouins);
-		Scene scene = new Scene(root);
+		chargeurFxml1er.fixeEcran(Interface.ECRAN_ACCUEIL);
+		
+		AnchorPane root = new AnchorPane();
+		root.getChildren().addAll(chargeurFxml1er);
+		Scene scene = new Scene(root,largeurFenetre,hauteurFenetre);
 	    primaryStage.setScene(scene);
+
+	    primaryStage.setResizable(false);
 	    primaryStage.show(); 
 	    }
+	
 	
 }

d15432359f96e399675bb098e112ad5ad9111c2d
kuhlkrein <superseb.07@hotmail.fr>
2017-05-11 13:47:30 +0200

    partie pour le gestionnaire

--- src/vue/Interface.java
+++ src/vue/Interface.java
@@ -30,7 +30,7 @@ public class Interface extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
-		GestionnaireEcransFxml gestionnaireEcransFXML = new GestionnaireEcransFxml();
+		GestionnaireEcransFxml gestionnaireEcransFXML = new GestionnaireEcransFxml(partie);
 		gestionnaireEcransFXML.chargeEcran(model.Proprietes.ECRAN_ACCUEIL, model.Proprietes.ECRAN_ACCUEIL_FXML);
 		gestionnaireEcransFXML.chargeEcran(model.Proprietes.ECRAN_REGLES, model.Proprietes.ECRAN_REGLES_FXML);
 		gestionnaireEcransFXML.chargeEcran(model.Proprietes.ECRAN_MODE, model.Proprietes.ECRAN_MODE_FXML);

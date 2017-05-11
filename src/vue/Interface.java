5427b96c9605013e378181cc342e58ab14f196c6
Noémi <Nomi@192.168.0.26>
2017-05-11 02:30:26 +0200

    ajout ecran Jeu

--- src/vue/Interface.java
+++ src/vue/Interface.java
@@ -34,8 +34,9 @@ public class Interface extends Application {
 		gestionnaireEcransFXML.chargeEcran(model.Proprietes.ECRAN_ACCUEIL, model.Proprietes.ECRAN_ACCUEIL_FXML);
 		gestionnaireEcransFXML.chargeEcran(model.Proprietes.ECRAN_REGLES, model.Proprietes.ECRAN_REGLES_FXML);
 		gestionnaireEcransFXML.chargeEcran(model.Proprietes.ECRAN_MODE, model.Proprietes.ECRAN_MODE_FXML);
+//		gestionnaireEcransFXML.chargeEcran(model.Proprietes.ECRAN_JEU, model.Proprietes.ECRAN_JEU_FXML);
         
-		gestionnaireEcransFXML.changeEcranCourant(model.Proprietes.ECRAN_ACCUEIL);
+		gestionnaireEcransFXML.changeEcranCourant(model.Proprietes.ECRAN_MODE_FXML);
         
         Group root = new Group();
         root.getChildren().addAll(gestionnaireEcransFXML);

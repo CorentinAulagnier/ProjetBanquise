9315c2fccb3ff058ecb3bd5ce370e73b0a346560
nthisse <nthisse@gmail.com>
2017-05-11 15:22:23 +0200

    maj ajout ecran Victoire (fxml, controleur et proprietes)

---
+++ src/controleur/ControleurVictoire.java
@@ -0,0 +1,67 @@
+package controleur;
+import vue.GestionnaireEcransFxml;
+import vue.EcranCourant;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+
+public class ControleurVictoire  implements Initializable, EcranCourant {
+	GestionnaireEcransFxml gestionnaireFxmlCourant;
+ 
+	 @FXML 
+	 private Button accueil;
+	 @FXML 
+	 private Button nouvelle_partie;
+	 @FXML 
+	 private Button nouvelle_manche;
+	
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    	accueil.setStyle(model.Proprietes.STYLE_NORMAL);
+    	nouvelle_partie.setStyle(model.Proprietes.STYLE_NORMAL);
+    	nouvelle_manche.setStyle(model.Proprietes.STYLE_NORMAL);
+    }
+    
+    public void fixeEcranParent(GestionnaireEcransFxml ecranParent){
+    	gestionnaireFxmlCourant = ecranParent;
+    }
+
+    @FXML
+    private void ouvrirPageMode(MouseEvent event){
+    	gestionnaireFxmlCourant.changeEcranCourant(model.Proprietes.ECRAN_MODE);
+    }
+    
+    @FXML
+    private void ouvrirPageJeu (MouseEvent event){
+    	gestionnaireFxmlCourant.changeEcranCourant(model.Proprietes.ECRAN_JEU);
+    }
+    
+    @FXML
+    private void ouvrirPageAccueil(MouseEvent event){
+    	gestionnaireFxmlCourant.changeEcranCourant(model.Proprietes.ECRAN_ACCUEIL);
+    }
+    
+    
+    
+/**-------------------------------------------PRESSION DES BOUTONS-------------------------------------------**/
+    
+    @FXML
+    public void boutonPresse(MouseEvent event){
+    	((Button) event.getTarget() ).setStyle(model.Proprietes.STYLE_PRESSED);
+    }
+
+    
+    
+    /**-------------------------------------------RELACHEMENT DES BOUTONS-------------------------------------------**/
+    
+    @FXML
+    public void boutonLache(MouseEvent event){
+    	((Button) event.getTarget() ).setStyle(model.Proprietes.STYLE_NORMAL);
+    }
+}
--- src/model/Proprietes.java
+++ src/model/Proprietes.java
@@ -9,6 +9,8 @@ public class Proprietes {
     public static final String ECRAN_MODE_FXML = "fxml/ModeJeu.fxml";
     public static final String ECRAN_JEU = "Jeu";
     public static final String ECRAN_JEU_FXML = "fxml/Jeu.fxml";
+    public static final String ECRAN_VICTOIRE = "Victoire";
+    public static final String ECRAN_VICTOIRE_FXML = "fxml/Victoire.fxml";
 
     public static final String STYLE_NORMAL = "-fx-background-color: transparent; -fx-padding: 5 5 5 5 ;";
     public static final String STYLE_PRESSED = "-fx-background-color: transparent; -fx-padding: 6 5 5 6;";

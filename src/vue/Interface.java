0cc0cd7ff21b6cb816d006251c5831471e9be0f8
kuhlkrein <superseb.07@hotmail.fr>
2017-05-17 14:43:09 +0200

    redimentionable

--- src/vue/Interface.java
+++ src/vue/Interface.java
@@ -4,7 +4,14 @@ import model.Partie;
 
 import java.io.File;
 
+import javafx.animation.ParallelTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -12,17 +19,22 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Interface extends Application {   
     
 	public static int largeurFenetre;
 	public static int hauteurFenetre;
 	public static Partie partie;  
+	public static int largeurInitiale;
+	public static int hauteurInitiale;
 
 	public static void creer(String[] args,Partie p,int h, int l) {
     	partie = p;
     	hauteurFenetre = h*100 +30;
     	largeurFenetre = l*100;
+    	largeurInitiale = largeurFenetre ;
+    	hauteurInitiale = hauteurFenetre;
         Application.launch(Interface.class, args);
     }
 	
@@ -38,7 +50,7 @@ public class Interface extends Application {
 		gestionnaireEcransFXML.chargeEcran(model.Proprietes.ECRAN_VICTOIRE, model.Proprietes.ECRAN_VICTOIRE_FXML);
 		
 		
-		/*String path = "src/ressources/decor/ping.mp3";
+		/*String path = "src/ressources/decor/christmas.mp3";
 		Media media = new Media(new File(path).toURI().toString());
 		gestionnaireEcransFXML.media = new MediaPlayer(media);
 		
@@ -57,8 +69,46 @@ public class Interface extends Application {
         primaryStage.setWidth(largeurFenetre);
         primaryStage.getIcons().add(new Image("ressources/decor/favicon.png"));
         primaryStage.setTitle("Pinguouins");
-        primaryStage.setResizable(false);
+        primaryStage.setResizable(true);
+        scene.setRoot(root);
+        primaryStage.setScene(scene);
+        gestionnaireEcransFXML.autosize();
+       // scene.setCamera();
         primaryStage.show();
+        
+        
+        primaryStage.widthProperty().addListener(new ChangeListener<Number>() {
+        	@Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
+                ScaleTransition st = new ScaleTransition(Duration.ONE, gestionnaireEcransFXML);
+                TranslateTransition tt = new TranslateTransition(Duration.ONE, gestionnaireEcransFXML);
+               
+                double ratio = (newSceneWidth.doubleValue() - largeurInitiale ) /2 ;
+                
+                st.setToX( newSceneWidth.doubleValue()/ largeurInitiale);
+                tt.setToX(ratio);
+            
+                ParallelTransition pt = new ParallelTransition(gestionnaireEcransFXML, st, tt);
+             	pt.play();
+             	
+        	}
+        });
+        
+      primaryStage.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
+                ScaleTransition st = new ScaleTransition(Duration.ONE, gestionnaireEcransFXML);
+                TranslateTransition tt = new TranslateTransition(Duration.ONE, gestionnaireEcransFXML);
+                
+                double ratio = (newSceneHeight.doubleValue()-hauteurInitiale) /2 ;
+                
+                st.setToY( newSceneHeight.doubleValue()/ hauteurInitiale);
+                tt.setToY( ratio );
+                
+             	ParallelTransition pt = new ParallelTransition(gestionnaireEcransFXML, st ,tt);
+    	     	pt.play();
+             	
+            }
+        });
+        
 	    }
 	
 

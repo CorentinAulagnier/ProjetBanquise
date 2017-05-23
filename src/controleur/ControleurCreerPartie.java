d503b0239b10427b91c3ea9319ec9d6c684a1c28
kuhlkrein <superseb.07@hotmail.fr>
2017-05-23 16:57:43 +0200

    création d'une partie fxml

---
+++ src/controleur/ControleurCreerPartie.java
@@ -0,0 +1,46 @@
+package controleur;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import model.Coordonnees;
+import reseau.PingouinClient;
+import vue.EcranCourant;
+import vue.GestionnaireEcransFxml;
+
+public class ControleurCreerPartie extends ControleurPere implements Initializable, EcranCourant {
+	
+	GestionnaireEcransFxml liste_Ecran;
+	
+    //zone menu
+    @FXML private ImageView imageSon, imageMusique;
+    @FXML AnchorPane optionbox;
+    @FXML Button roue;
+	
+    /**
+	 * initialisation des parametres au chargement du noeud fxml associe a ce controleur
+	 */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+    
+    /**
+     * implementation demande par l'interface EcranCourant : vide car n'a pas d'utilite ici
+     */
+	public void miseAjour(){}
+	 
+	/**
+	 * implementation demande par l'interface EcranCourant : met a jour le noeud fxml parent associe a ce controleur
+	 */
+    public void fixeEcranParent(GestionnaireEcransFxml ecranParent){
+    	liste_Ecran = ecranParent;
+    }
+}

a3f186b6ae167c7e30b21b70fcedfb44a8e446db
kuhlkrein <superseb.07@hotmail.fr>
2017-05-24 11:18:53 +0200

    création de partie fxml

--- src/controleur/ControleurCreerPartie.java
+++ src/controleur/ControleurCreerPartie.java
@@ -8,8 +8,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import model.Coordonnees;
 import reseau.PingouinClient;
@@ -24,6 +26,9 @@ public class ControleurCreerPartie extends ControleurPere implements Initializab
     @FXML private ImageView imageSon, imageMusique;
     @FXML AnchorPane optionbox;
     @FXML Button roue;
+    @FXML StackPane pilejaune;
+    @FXML Button droitjaune;
+    int indice = model.Proprietes.JOUEUR;;
 	
     /**
 	 * initialisation des parametres au chargement du noeud fxml associe a ce controleur
@@ -58,5 +63,86 @@ public class ControleurCreerPartie extends ControleurPere implements Initializab
 		}
 		*/
     	
+    }
+    
+    @FXML
+    public void fleche(MouseEvent event){
+    	
+    	pilejaune.getChildren().get(indice).setVisible(false);
+    	if (((Button) event.getTarget() ) == droitjaune ){							// 0 1 2 3 4
+    		indice = (indice+1)%5;
+    		if (indice == 0){indice=1;}
+    	}else{
+    			indice= (indice+4)%5;
+    			if (indice == 0){indice=4;}
+    	}
+    	pilejaune.getChildren().get(indice).setVisible(true);  	
+    }
+    
+    /**
+     * gere la modification du volume de la musique
+     * @param event event evenement souris attendu : clic
+     */
+    @FXML
+    private void gererMusique(MouseEvent event){
+    	changerMusique(imageMusique , liste_Ecran.media, liste_Ecran);
+    }
+    
+    /**
+     * gere la modification des bruitages
+     * @param event event evenement souris attendu : clic
+     */
+    @FXML
+    private void gererSon(MouseEvent event){
+    	changerSon(imageSon, liste_Ecran);
+    }    
+    
+    /**
+     * gere l'ouverture ou la fermeture du menu roue
+     * @param event evenement souris attendu : clic
+     */
+    @FXML
+    public void boutonOption(MouseEvent event){
+    	if (optionbox.isDisable()){
+    		majBoutonMusique(liste_Ecran,imageMusique);
+    		majBoutonSon(liste_Ecran,imageSon);;
+    		optionOuvrirRoue(optionbox, roue) ;
+    	}else{
+    		 optionFermerRoue(optionbox, roue) ;
+    	}
+    }
+    
+      
+    
+    
+    @FXML
+    private void quitter(MouseEvent event){
+    	nettoyerMenu(optionbox, roue);
+    	alert_quitter(liste_Ecran);
+    }
+    
+    /**
+     * change d'ecran pour celui des regles
+     * @param event evenement souris attendu : clic
+     */
+    @FXML
+    private void ouvrirPageRegle(MouseEvent event){
+    	nettoyerMenu(optionbox, roue);
+    	liste_Ecran.dernierePage = model.Proprietes.ECRAN_CREATION;
+    	liste_Ecran.changeEcranCourant(model.Proprietes.ECRAN_REGLES);
+    }
+    
+    /**
+     * 
+     * @param event
+     */
+    @FXML
+    public void textEntre(KeyEvent event){
+    	String nom = ((TextField) event.getTarget() ).getText();
+    	if ( nom.length() <= 25){
+    	}else{
+    		((TextField) event.getTarget()).deleteText(25, nom.length());
+    		((TextField) event.getTarget()).setCenterShape(true);
+    	}
     }
 }

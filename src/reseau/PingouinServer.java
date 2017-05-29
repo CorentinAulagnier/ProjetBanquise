95aa2c08e7ee531311133176f624981a1e02a50b
corentin <corentinaulagnier@hotmail.fr>
2017-05-29 12:20:06 +0200

    hg

--- src/reseau/PingouinServer.java
+++ src/reseau/PingouinServer.java
@@ -153,7 +153,7 @@ public class PingouinServer extends Thread{
                     }
                     //Maj phaseConnexion
     	            nbClients++;
-                	while (m.partie.joueurs[nbClients].getClass() == IA.class) { nbClients ++; }
+                	while (nbClients != m.partie.nbJoueurs && m.partie.joueurs[nbClients].getClass() == IA.class) { nbClients ++; }
 
     	            if(nbClients == m.partie.nbJoueurs) {// || m.partie.joueurs[nbClients].getClass()==IA.class) {
     	            	phaseConnexion = false;
